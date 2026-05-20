92b2a10acf33fb8fc3fdd1f2a8cfebfdfa25d0e0
JimingYang25 <2132720978@qq.com>
2026-05-20 09:01:00 +0800

    model_reb

--- src/terrain_classifier_pkg/terrain_classifier_pkg/Vogue_Ming.py
+++ src/terrain_classifier_pkg/terrain_classifier_pkg/Vogue_Ming.py
@@ -5,13 +5,13 @@ import torch.nn as nn
 
 class Vogue_Ming(nn.Module):
     """Multitask MLP:Shared labelencoder + Classification header + Regression header(slope,roughness,height variation)"""
-    def __init__(self, input_dim=40, hidden_dims=[480,240,120,40], num_classes=9, dropout=0.4):
+    def __init__(self, input_dim=40, hidden_dims=[240,40], num_classes=9, dropout=0.2):
         super().__init__()
-        self.frame_branch=nn.Sequential(nn.Linear(10,60),nn.BatchNorm1d(60),nn.ReLU(),nn.Dropout(dropout),)
-        self.window_branch=nn.Sequential(nn.Linear(30,180),nn.BatchNorm1d(180),nn.ReLU(),nn.Dropout(dropout))
+        self.frame_branch=nn.Sequential(nn.Linear(10,80),nn.BatchNorm1d(80),nn.ReLU(),nn.Dropout(dropout),)
+        self.window_branch=nn.Sequential(nn.Linear(30,240),nn.BatchNorm1d(240),nn.ReLU(),nn.Dropout(dropout))
         # shared layers
         layers = []
-        prev_dim = 240
+        prev_dim = 320
         
         for h in hidden_dims:
             layers.append(nn.Linear(prev_dim, h))
@@ -21,9 +21,9 @@ class Vogue_Ming(nn.Module):
             prev_dim = h
         self.shared = nn.Sequential(*layers)
         
-
+        # classification header
         self.class_head = nn.Linear(prev_dim, num_classes)
-       
+        # regression header
         self.slope_head = nn.Linear(prev_dim, 1)
         self.rough_head = nn.Linear(prev_dim, 1)
         self.elev_head = nn.Linear(prev_dim, 1)
